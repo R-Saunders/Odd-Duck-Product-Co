6aca79260d69ec7391dad04f76b0be470c449a2a
Rich Saunders <richsaunders@protonmail.com>
2022-09-05 14:48:16 +0100

    created start of constructor

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class products {
+  constructor(name, img, views, clicks) {
+    this.name = name;
+    this.img = img;
+    this.views = views;
+    this.clicks = clicks;
+  }
+  
+}
